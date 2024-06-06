fd3845714d2174e0bf04bfd65ba52a27ebb8fe02
Sebastian Rode <sebastian@djangsters.de>
2024-06-06 11:59:58 +0200

    addform

--- frontend/src/components/SimpleComponent.tsx
+++ frontend/src/components/SimpleComponent.tsx
@@ -1,15 +1,34 @@
-import { useState } from "react";
+import React, { useState } from "react";
 import { helloApiAdd } from "../../api/generated-api"
 import { useCookies } from 'react-cookie'
+import { send } from "process";
 
-const [result, setResult] = useState([]);
 
 
 const SimpleComponent = () =>{
+    const [result, setResult] = useState([])
+
+    function sendForm(formData){
+        fetchAdd(formData.x, formData.y)
+        console.log("ttt")
+    }
+
+    async function fetchAdd(x: number, y: number) {
+        const [cookies] = useCookies(['csrftoken'])
+        const response = await fetch('/api/hello/add', {
+            method: "POST",
+            headers: { 'X-CSRFToken': cookies.csrftoken},
+            body: JSON.stringify({x: x, y:y})
+        }).then(res => {
+            const data = res.json()
+            setResult(data.result)
+        })
+    }
+
     return(
         <div>
             <h3>Add API call</h3>
-            <form action="/action_page.php">
+            <form action={sendForm}>
                 <input type="text" name="x" />
                 <input type="text" name="y" />
                 <button type="submit">=</button>
@@ -18,14 +37,5 @@ const SimpleComponent = () =>{
     )
 }
 
-async function fetchAdd(x: number, y: number) {
-    const [cookies] = useCookies(['csrftoken'])
-    const response = await fetch('/api/hello/add', {
-        method: "POST",
-        headers: { 'X-CSRFToken': cookies.csrftoken},
-        body: JSON.stringify({x: x, y:y})
-    }).then(res => res.json())
-
-}
 
 export default SimpleComponent
